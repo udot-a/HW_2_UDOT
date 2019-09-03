76441e3958322c2f58445e47f78600a2f86783cb
Udot Andrii <udot.dron@gmail.com>
2019-09-03 15:41:05 +0300

    hw2

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-let button = document.getElementById('button');
-let input = document.getElementById('input');
-button.onclick = (event) => {
-    if (input.value != '') {
-        
-        alert(`Числа значение к-х больше 50:
-        `+input.value.split(',').map((i)=>Number(i)).filter((i) => (i > 50)))
-        // arr.filter((i) => (i > 3))
+// Первое задание
+let firstLetterButton = document.getElementById('firstLetterButton');
+let inputString = document.getElementById('inputString');
+let firstLetterList = document.getElementById('firstLetterList');
+firstLetterButton.onclick = (event) => {
+    if (inputString.value != '') {
+        firstLetterList.innerHTML = inputString.value.split(' ').map((word) => word[0]).join('')
     }
-    else alert('Введите числа!!!')
+    else alert('Введите что-нибудь!!!')
+};
+// Второе задание
+let determineNumb = document.getElementById('determineNumb');
+let inputNumber = document.getElementById('inputNumber');
+let result = document.getElementById('result');
+
+function isNumber(n) {
+    if ((n == null) || (n + '' === '') || (isNaN(Number(n)))) return 'Неверный тип данных!!!'
+    var date = new Date();
+    return `${date.toLocaleDateString()}, ${date.toLocaleTimeString()}`
+}
+
+determineNumb.onclick = (event) => {
+    result.innerHTML = isNumber(inputNumber.value)
+    debugger;
+
 };
